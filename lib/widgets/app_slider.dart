37daf6d95617696889eeaf14f0579ab72f7d1c46
abolfazl <abolfazl2277999@gmail.com>
2025-12-12 17:32:26 +0330

    app slider

--- lib/widgets/app_slider.dart
+++ lib/widgets/app_slider.dart
@@ -1,17 +1,82 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:watch_store/res/dimes.dart';
 
-// url https://ticktackgallery.com/media/wysiwyg/ticktackbanner/men.png
+final List<String> imgList = [
+  "https://ticktackgallery.com/media/wysiwyg/ticktackbanner/men.png",
+  "https://ticktackgallery.com/media/wysiwyg/ticktackbanner/Casio_desktop_banner_1_.png",
+  "https://ticktackgallery.com/media/wysiwyg/ticktackbanner/Orient_banner_desktop_1.png",
+  "http://ticktackgallery.com/media/wysiwyg/ticktackbanner/romanson_banner_desktop_1.png",
+  "https://ticktackgallery.com/media/wysiwyg/ticktackbanner/Seiko_banner_desktop_1.png",
+];
+
 class AppSlider extends StatefulWidget {
   const AppSlider({super.key});
-
   @override
   State<AppSlider> createState() => _AppSliderState();
 }
 
 class _AppSliderState extends State<AppSlider> {
+  final CarouselSliderController _controller = CarouselSliderController();
+  int _current = 0;
   @override
   Widget build(BuildContext context) {
-    return Container(height: 222.h, width: 355.w, color: Colors.red);
+    return SizedBox(
+      height: 260.h,
+      width: 355.w,
+      child: Column(
+        children: [
+          Expanded(
+            child: CarouselSlider(
+              carouselController: _controller,
+              items: imgList
+                  .map(
+                    (e) => Padding(
+                      padding: EdgeInsets.all(AppDimes.smallRadius),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(AppDimes.avatar),
+                        child: Image.network(e, fit: BoxFit.cover),
+                      ),
+                    ),
+                  )
+                  .toList(),
+              options: CarouselOptions(
+                viewportFraction: 1.0,
+                autoPlay: true,
+                enlargeCenterPage: false,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: imgList.asMap().entries.map((e) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    _controller.animateToPage(e.key);
+                  },
+                  child: Container(
+                    width: 8,
+                    height: 8,
+                    margin: EdgeInsets.symmetric(horizontal: 2),
+                    decoration: BoxDecoration(
+                      color: _current == e.key ? Colors.black : Colors.grey,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
