8ea1ec73492ed480e0390e3b7ebb5347612e1d69
abolfazl <abolfazl2277999@gmail.com>
2026-01-03 23:58:04 +0330

    add constants

---
+++ lib/data/constants.dart
@@ -0,0 +1,6 @@
+class Endpoints {
+  static const String baseUrl = "https://watchstore.sasansafari.com/";
+  static const String sendSMS = "${baseUrl}public/api/v1/send_sms";
+  static const String checkSMSCode = "${baseUrl}public/api/v1/check_sms_code";
+  static const String register = "${baseUrl}public/api/v1/register";
+}
